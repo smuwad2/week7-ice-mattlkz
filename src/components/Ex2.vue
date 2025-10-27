b9ccbd263d80565a3a267860529cc802dec11621
mattlkz <161346061+mattlkz@users.noreply.github.com>
2025-10-27 10:19:19 +0000

    wk10

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -1,24 +1,29 @@
 <script>
     export default { 
-        // Add code in appropriate places
         data() {
             return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
+                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+                newItem: ''
             }
-            
+        },
+        methods: {
+            add() {
+                // console.log('in add')
+                this.items.push(this.newItem)
+                this.newItem = ''
+            }
         }
     }
 </script>
 
 <template>
-
     <h2>Shopping Cart</h2>
-    <ul v-for ="item in items">
-        <li>
+    <ul>
+        <li v-for="(item, i) in items">
             {{item}}
-        </li>
+        <button @click="items.splice(i, 1)">Delete!</button> </li>
     </ul>
-    Student Name: <input type="text" v-model="studName"></input>
-    <!-- TODO: Add Code Here -->
+    <input @keyup.enter="add" v-model="newItem"/>
+    <button @click="add">Add!</button>    
    
 </template>
